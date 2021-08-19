b5cc4b3eb820b369350cddbc6f70df98eefc3d7b
Neocky <Neocky@users.noreply.github.com>
2021-08-19 11:02:41 +0200

    Version 1.1.0
    Added threading for more speed

--- choco-runner.ps1
+++ choco-runner.ps1
@@ -3,9 +3,13 @@
 Rapid fast multi package installer for chocolatey packages
 
 .DESCRIPTION
-Installs programs super fast with chocolatey and installs chocolatey if needed.
-More than one program can be given to as param -package to run the installation paralell.
+Installs/upgrades programs super fast with chocolatey and installs chocolatey if needed.
+More than one program can be given to as param -package to run the installation in different threads.
 This script needs to be run as administrator.
+
+.EXAMPLE
+.\choco-runner -package 7zip.install,firefoxesr -removeChocoAfterwards
+Installs 7zip & firefox and removes Chocolatey afterwards.
 
 .PARAMETER package
 A list of packages which should be installed/upgraded. Seperate it with a ",".
@@ -13,9 +17,15 @@ A list of packages which should be installed/upgraded. Seperate it with a ",".
 .PARAMETER removeChocoAfterwards
 Removes Chocolatey afterwards when this parameter is given.
 
+.PARAMETER keepChocoAfterwards
+Keeps Chocolatey afterwards when this parameter is given.
+
+.PARAMETER threads
+Maximum numbers of threads (Default=256)
+
 .NOTES
 Author: Neocky
-Version: 1.0.0
+Version: 1.1.0
 
 .LINK
 https://github.com/Neocky/choco-runner
@@ -26,8 +36,10 @@ https://github.com/Neocky/choco-runner
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory=$False)][array]$package,
-    [Parameter(Mandatory=$False)][switch]$removeChocoAfterwards
+    [Parameter(Mandatory=$False,Position=0)][array]$package,
+    [Parameter(Mandatory=$False)][switch]$removeChocoAfterwards,
+    [Parameter(Mandatory=$False)][switch]$keepChocoAfterwards,
+    [Parameter(Mandatory=$False)][Int32]$threads = 256
 )
 
 
@@ -42,11 +54,13 @@ function writeTitle {
 function checkParam {
     [CmdletBinding()]
     param (
-        [Parameter()][array]$programsToInstall
+        [Parameter()][array]$programsToInstall,
+        [Parameter()][Boolean]$removeChocolateyAfter,
+        [Parameter()][Boolean]$keepChocolateyAfter
         )
     
     if (($programsToInstall).Count -eq 0) {
-        Write-Host "Package name is required. Please use the parameter: -package PACKAGENAME1,PACKAGENAME2" -ForegroundColor Red -BackgroundColor Black
+        Write-Warning "Package name is required. Please use the parameter: -package PACKAGENAME1,PACKAGENAME2"
         Write-Output "Here is a list with all available packages to install: https://community.chocolatey.org/packages"
         @(
             "POPULAR PACKAGES:"
@@ -56,6 +70,11 @@ function checkParam {
             "├─ 7zip.install",
             "└─ office365business"
             ) -join "`n" | Write-Output
+        Exit 1
+    }
+
+    if (($removeChocolateyAfter -eq $True) -and ($keepChocolateyAfter -eq $True)) {
+        Write-Warning "Can't use both parameters 'removeChocoAfterwards' & 'keepChocoAfterwards' at once"
         Exit 1
     }
 }
@@ -85,7 +104,13 @@ function removeChocolatey {
 
 
 function checkChocolateyInstall {
-    if (-not (Test-Path $env:ChocolateyInstall)) {
+    try {
+        $chocoInstalled = Test-Path $env:ChocolateyInstall -ErrorAction Stop
+    } catch {
+        $chocoInstalled = $False
+    }
+
+    if ($chocoInstalled -eq $False) {
         Write-Output "Chocolatey wasn't found on the system."
         installChocolatey
         
@@ -108,56 +133,71 @@ $installer = {
         param (
             [Parameter()][array]$programsToInstall
             )
-        Write-Host "#### " -NoNewline -ForegroundColor Gray; Write-Host $programsToInstall -ForegroundColor Magenta -NonewLine; Write-Host " ####" -ForegroundColor Gray
         choco upgrade $programsToInstall -y --limitoutput --no-progress
         }
 
     try {
-        installPackage -programsToInstall $chocoProgram
+        installPackage -programsToInstall $chocoProgram -ErrorAction Stop
     } catch {
-        Write-Host "● " -ForegroundColor Red -NonewLine; Write-Host "Couldn't install: " -ForegroundColor Red -NoNewline; Write-Host $chocoProgram
+        Write-Host "● " -ForegroundColor Red -NonewLine; Write-Host "Couldn't install: " -ForegroundColor Red -NoNewline; Write-Host "$chocoProgram"
         Exit
     }
-    Write-Host "● " -ForegroundColor Green -NonewLine; Write-Host "Successfully installed: " -ForegroundColor Green -NoNewline; Write-Host $chocoProgram
-    Write-Host ""
+    Write-Host "● " -ForegroundColor Green -NonewLine; Write-Host "Successfully installed: " -ForegroundColor Green -NoNewline; Write-Host "$chocoProgram"
 }
 
 
+checkParam -programsToInstall $package -removeChocolateyAfter $removeChocoAfterwards -keepChocolateyAfter $keepChocoAfterwards
 writeTitle
-checkParam -programsToInstall $package
 checkChocolateyInstall
 Write-Host ""
 Write-Host "Packages to install: " -ForegroundColor Green
 Write-Output $package
 Write-Host ""
 
-
-$jobs = @()
+$RunspacePool = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateRunspacePool(1, $threads, $Host)
+$RunspacePool.Open()
+[System.Collections.ArrayList]$jobs = @()
 Foreach ($packageToInstall in $package) {
-    $jobs += Start-Job -ArgumentList $packageToInstall -ScriptBlock $installer
+    $scriptParams = @{
+        chocoProgram = $packageToInstall
+    }
+    $job = [System.Management.Automation.PowerShell]::Create().AddScript($installer).AddParameters($scriptParams)
+    $job.RunspacePool = $RunspacePool
+    $jobObj = [pscustomobject] @{
+        Pipe = $job
+        Result = $job.BeginInvoke()
+    }
+
+    [void]$jobs.Add($jobObj)
 }
 
 
-# Spinner
-$spinnerAnimation = "/-\|/-\|"
-$i = 0
-while (($jobs.State -eq "Running") -and ($jobs.State -ne "NotStarted"))
-{
-	Write-Host `r($spinnerAnimation[$i]) -NoNewline; Write-Host " Installing packages..." -NoNewline
-	$i++
-	if ($i -ge $spinnerAnimation.Length) {
-		$i = 0
-	}
-	Start-Sleep -Milliseconds 100
-}
+$i = 0 # needed for progress bar
+Do {
+    Write-Progress -Activity "Installing choco packages..." -Status ("[ $i / " + $package.Count + " ] packages finished") -PercentComplete ($i/$package.Count * 100)
+    $unfinishedJobs = $jobs | Where-Object -FilterScript {$_.Result.IsCompleted}
 
-Write-Host ""
-Receive-Job $jobs -AutoRemoveJob -Wait
+    if($null -eq $unfinishedJobs) {
+        Start-Sleep -Milliseconds 250
+        continue
+    }
 
+    foreach($job in $unfinishedJobs) {
+        $jobOutput = $job.Pipe.EndInvoke($job.Result)
+        $job.Pipe.Dispose()
+        $jobs.Remove($job)
+        Write-Output $jobOutput
+        $i++
+    }
+} While ($jobs.Count -gt 0)
 
-if ($removeChocoAfterwards -ne $True) {
+# close all threads
+$RunspacePool.Close()
+$RunspacePool.Dispose()
+
+if (($removeChocoAfterwards -ne $True) -and ($keepChocoAfterwards -ne $True)) {
     $removeChocolateyConfirmation = Read-Host "Remove Chocolatey? (y/n)"
 }
-if (($removeChocolateyConfirmation -eq "y") -or ($removeChocoAfterwards -eq $True)) {
+if (($removeChocolateyConfirmation -eq "y") -or ($removeChocoAfterwards -eq $True) -and ($keepChocoAfterwards -ne $True)) {
     removeChocolatey
 }
